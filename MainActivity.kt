06a1fc1ea4bca8d292233bdc04b40c3b82d5bd73
Monaratuliu05 <monaratuliumona05@gmail.com>
2024-10-07 20:00:56 +0700

    first commit.

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+package wani.polbeng.ac.id.androidkotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvCounter: TextView
+    private lateinit var btnAddNumber: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvCounter = findViewById(R.id.tvCounter)
+        btnAddNumber = findViewById(R.id.btnAddNumber)
+        tvCounter.text = "1"
+    }
+
+    fun addNumber(v: View) {
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
+
+        fun SubtractNumber(v: View) {
+            val currVal = tvCounter.text.toString().toInt()
+            val nextVal = currVal + 0
+            tvCounter.text = nextVal.toString()
+
+            fun ResetNumber(v: View) {
+                val currVal = tvCounter.text.toString().toInt()
+                val nextVal = currVal + 1
+                tvCounter.text = nextVal.toString()
+
+            }
+        }
+    }
+}
